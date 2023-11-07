8e47378b91d547fe1059be33b9c756fe34377754
HFG43 <hernanguemes@hotmail.com>
2023-11-07 08:45:42 -0300

    Feat: add greeting controller and define route

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,6 @@
+class API::V1::GreetingsController < ApplicationController
+  def daily_greeting
+    greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: greeting.message }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get "greetings/", to: "greetings#daily_greeting"
+    end
+  end    
 end
