122f36473b0b90f9af4527b7b3e346dccb479bb8
HFG43 <hernanguemes@hotmail.com>
2023-11-07 08:48:03 -0300

    Misc: correct class typo in GreetingController

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::GreetingsController < ApplicationController
+class Api::V1::GreetingsController < ApplicationController
   def daily_greeting
     greeting = Greeting.order('RANDOM()').first
     render json: { greeting: greeting.message }
